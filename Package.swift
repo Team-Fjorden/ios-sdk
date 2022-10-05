0d435f3ca7bf93ff69735bb515538391b4f9db39
Florian Bürger <florian@fjorden.co>
2022-10-05 17:43:57 +0200

    Try new binary

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 import PackageDescription
 
-let version = "1.1.3"
-let checksum = "acb0ee9723b0c59e6f1098a2f8cfb9be9b63fb40682a940bdace0ce6750f853e"
+let version = "1.1.4"
+let checksum = "d722646bed700e7d7a2535d2b8595b1ecd76219c00eafde2a6b245dd7fca842a"
 
 let package = Package(
     name: "FjordenSDK",
