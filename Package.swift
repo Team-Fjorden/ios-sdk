72a9b8c96a559377e386c83bf7758e13479efd64
Florian Bürger <florian@fjorden.co>
2022-10-13 18:29:05 +0200

    Release 1.1.4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.6
 import PackageDescription
 
-let version = "1.1.3"
-let checksum = "acb0ee9723b0c59e6f1098a2f8cfb9be9b63fb40682a940bdace0ce6750f853e"
+let version = "1.1.4"
+let checksum = "d089835657ca3ba0a1ddd1f4c01717a95a87f2c50cdeb3bd71fcf865741af6f3"
 
 let package = Package(
     name: "FjordenSDK",
