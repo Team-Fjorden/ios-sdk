41010c16413b3a5d1eca2f6847abf1fddf975cd1
Florian Bürger <florian@fjorden.co>
2023-11-14 12:42:04 +0100

    Release 1.3.2

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.6
 import PackageDescription
 
-let version = "1.3.1"
+let version = "1.3.2"
 let checksum = "2fc3aa7d99ef4a18ac0b43aaf7d450aa92b1db85a35df673c58ecea1a3ec2301"
 
 let package = Package(
