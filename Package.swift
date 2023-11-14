3df7bf9fe134c4e4aba2af62ab80a858dab0b06e
Florian Bürger <florian@fjorden.co>
2023-11-14 12:45:28 +0100

    Fix checksum

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let version = "1.3.2"
-let checksum = "2fc3aa7d99ef4a18ac0b43aaf7d450aa92b1db85a35df673c58ecea1a3ec2301"
+let checksum = "86692e959cd324b00ab5fb4412f7127d0594949e4225500eb0507c819d597293"
 
 let package = Package(
     name: "FjordenSDK",
